eaaa5e1e9af80d3ffe47bc3f82f6b41c2de2327a
danteramirez <danterldk@gmail.com>
2016-04-26 13:26:28 -0500

    centropre

---
+++ conexion.php
@@ -0,0 +1,36 @@
+
+<?php
+    $host="127.0.0.1";
+    $port="5432";
+    $user="postgres";
+    //$pass="";
+    $dbname="BD_CentroPre";
+
+    $connect = pg_connect("host=$host port=$port user=$user dbname=$dbname");
+
+    //consulta
+    /*$result = pg_query($connect, "select * from persona;");
+    while ($row = pg_fetch_row($result)) {
+      echo "Author: $row[0] Email: $row[1]";
+      echo "<br/>\n";
+    };
+    $bandera = pg_query($connect, "    INSERT INTO persona(
+            nombre_per, apepat_per, apemat_per, email, id_asignatura, usuario,
+            clave, dni)
+    VALUES ('carlos', 'benavides', 'montenro', 'carn@gmail.com', 1, 'vimcemt',
+            'acuario72', '71433272');");
+
+
+
+
+    if(!$connect)
+        echo "<p><i>No me conecte</i></p>";
+    else
+        echo "<p><i>Me conecte</i></p>";
+
+    pg_close($connect);
+
+?>
+    </body>
+</html>
+*/
---
+++ recibidos.php
@@ -0,0 +1,27 @@
+<?php
+  include 'conexion.php';
+    /*$nombre= $_POST['nombres'];
+    $apellido_paterno= $_POST['apellido_paterno'];
+    $apellido_materno= $_POST['apellido_materno'];
+    $dni= $_POST['dni'];
+    $ruc= $_POST['ruc'];
+    $direccion= $_POST['direccion'];
+    $email= $_POST['email'];
+
+    echo $nombre . $apellido_paterno . $apellido_materno . $dni . $ruc . $direccion . $email ;
+
+    $bandera = pg_query($connect, "    INSERT INTO persona(
+          nombre_per, apepat_per, apemat_per, email, id_asignatura, usuario,
+          clave, dni)
+          VALUES ('$nombre', '$apellido_paterno', '$apellido_materno', '$email', 1, 'gdfgd',
+          'sfsr', '$dni');");
+      */
+      $name_idioma[] = json_decode($_POST['name_idioma']);
+      $nivel_idioma[] =json_decode($_POST['nivel_idioma']);
+      $fecha_inicio_idioma[] = json_decode($_POST['fecha_inicio_idioma']);
+      $fecha_fin_idioma[] = json_decode($_POST['fecha_fin_idioma']);
+
+          foreach ($name_idioma as $key => $value) {
+            echo $value;
+              }
+ ?>
